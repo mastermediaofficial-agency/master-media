f124dd777d95ffdd0a638c249c06cf7da8267fac
krishna-mongoose <krishnagupta.mongoosetech@gmail.com>
2026-02-18 18:51:08 +0530

    fix: code clean -1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import MosaicCollageDemo from "@/src/components/landing/CollageDemo";
-// import CollageDemo from "@/src/components/landing/CollageDemo";
+
 import Hero from "@/src/components/landing/Hero";
 import JoinUI from "@/src/components/landing/JoinUI";
 import Mastery from "@/src/components/landing/Mastery";
@@ -9,15 +8,13 @@ import PartnerBelt from "@/src/components/landing/PartnerBelt";
 import Reviews from "@/src/components/landing/Reviews";
 import Services from "@/src/components/landing/Services";
 import VideoGallery from "@/src/components/landing/VideoGallery";
-// import WhatWeDo from "@/src/components/landing/WhatWeDo";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
       {/* <WhatWeDo /> */}
-      <Services />
-      {/* <MosaicCollageDemo /> */}
+      <Services />   
       <Media/>
       <PartnerBelt/>
       <VideoGallery />
